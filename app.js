e02476ad7105490ac1bb945ca39117574f8e6d12
SpreadUrWingz <scrotumtoast@gmail.com>
2024-09-09 18:11:38 -0700

    file creation

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const port = 3000;
+
+const pokemon = require('pokemontcgsdk');
+pokemon.configure({apiKey:'dbc834fd-849c-44ff-ad03-77ac3422b560'})
+
+
+app.get('/sets', async (req, res) => {
+    try {
+
+        console.log(`server called by ${req.ip}`);
+
+        pokemon.set.all({orderBy: "releaseDate", select: "name,id"})
+        .then((sets) => {
+            res.json(sets);
+            console.log('loaded sets\n~~~~~~~~~~~~~~~~~~~~~');
+        })
+        }
+        catch (error) {
+            // Handle any errors that occur
+            console.error('Error fetching data:', error);
+            res.status(500).send('Internal Server Error');
+        }
+});
+
+app.get('/cards/:name', async (req, res) => {
+    try {
+
+        console.log(`server called by ${req.ip}`);
+        console.log(`set to retrieve: ${req.params['name']}`);
+
+        
+        pokemon.card.all({ q: `set.id:${req.params['name']}`, orderBy:'number',
+                        select: "rarity,name,number,images,set"})
+            .then((cards) => {
+                res.json(cards);
+                console.log(`${req.params['name']} cards sent\n~~~~~~~~~~~~~~~~~~~~~`);
+
+            })
+        }
+        catch (error) {
+            // Handle any errors that occur
+            console.error('Error fetching data:', error);
+            res.status(500).send('Internal Server Error');
+        }
+});
+
+
+app.listen(port, () => {
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n
+        Server is running on http://localhost:${port}
+        \n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+});
